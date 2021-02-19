48f2b79cd3b92d83ef26805530c5a3201e9d4433
Sergey Gusev <forletterstogusev@gmail.com>
2021-02-19 20:12:44 +0200

    HW 1.5 done

--- app/code/Training/TestOM/Model/Test.php
+++ app/code/Training/TestOM/Model/Test.php
@@ -10,17 +10,20 @@ class Test
     private $arrayList;
     private $name;
     private $number;
+    private $managerFactory;
 
     public function __construct(
         \Training\TestOM\Model\ManagerInterface $manager,
         $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory
     ) {
         $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
+        $this->managerFactory = $managerFactory;
     }
 
     public function log()
@@ -32,5 +35,8 @@ class Test
         print_r($this->number);
         echo '<br>';
         print_r($this->arrayList);
+        echo '<br>';
+        $newManager = $this->managerFactory->create();
+        print_r(get_class($newManager));
     }
 }
