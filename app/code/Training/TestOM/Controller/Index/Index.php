2207c679902ea681de2d884c9bf60249f0c07168
Sergey Gusev <forletterstogusev@gmail.com>
2021-02-20 00:29:29 +0200

    HW 1.6 done

--- app/code/Training/TestOM/Controller/Index/Index.php
+++ app/code/Training/TestOM/Controller/Index/Index.php
@@ -4,17 +4,17 @@ namespace Training\TestOM\Controller\Index;
 
 class Index implements \Magento\Framework\App\ActionInterface
 {
-    private $testOM;
+    private $playWithTest;
 
     public function __construct(
-        \Training\TestOM\Model\Test $testOM
+        \Training\TestOM\Model\PlayWithTest $playWithTest
     ) {
-        $this->testOM = $testOM;
+        $this->playWithTest = $playWithTest;
     }
 
     public function execute()
     {
-        $this->testOM->log();
+        $this->playWithTest->run();
         exit;
     }
 }
