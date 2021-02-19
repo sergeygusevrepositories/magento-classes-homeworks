0577a32fa8c3695b534b76996d520276b8788d02
Sergey Gusev <forletterstogusev@gmail.com>
2021-02-19 19:17:47 +0200

    HW 1.3

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Training\AdditionalTestOM\Model;
+
+
+class ManagerCustom implements \Training\TestOM\Model\ManagerInterface
+{
+    public function create()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+}
